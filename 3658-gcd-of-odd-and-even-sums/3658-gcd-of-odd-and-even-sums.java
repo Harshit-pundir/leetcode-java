8bb3e7f4e518b90bc9fd7cafa69807adcb0b9c40
Harshit Pundir <harshitpundir36@gmail.com>
2026-07-15 08:22:02 +0530

    Time: 1 ms (83.11%), Space: 42.4 MB (86.19%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -9,22 +9,16 @@ class Solution {
 
         int count =0;
         int i =1;
+        int j =0;
 
         while(count <= n){
             sumOdd += i;
             i+=2;
+
+            sumEven += j;
+            j+=2;
             count++;
         }
-
-        count =0;
-        i =0;
-
-        while(count <= n){
-            sumEven += i;
-            i+=2;
-            count++;
-        }
-
         return gcd(sumOdd,sumEven)-1;
     }
 }
