2d4051e317555b09976ec075501cdf67c39320f0
Harshit Pundir <harshitpundir36@gmail.com>
2026-07-21 10:03:46 +0530

    Time: 20 ms (78.48%), Space: 54 MB (52.03%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,20 +1,23 @@
 class Solution {
-    
-    public int solve(String text , int i , String text2 , int j,int[][] dp){
-        if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(text.charAt(i) == text2.charAt(j)) return dp[i][j] = 1 + solve(text, i-1, text2, j-1, dp);
-
-        return dp[i][j] = Math.max(solve(text,i-1,text2,j,dp) , solve(text , i , text2,j-1,dp));
-    }
     public int longestCommonSubsequence(String text1, String text2) {
+
         int n = text1.length();
         int m = text2.length();
 
-        int[][] dp = new int[n][m];
-        for(int[] arr : dp){
-            Arrays.fill(arr,-1);
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+
+            }
         }
-        return solve(text1,n-1,text2 , m-1,dp);
+
+        return dp[n][m];
     }
 }
