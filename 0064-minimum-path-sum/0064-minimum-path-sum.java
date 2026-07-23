8bf9f0ac55d280701a0c167d217ef3e2ed297e5f
Harshit Pundir <harshitpundir36@gmail.com>
2026-07-23 06:37:59 +0530

    Time: 1 ms (99.93%), Space: 50.3 MB (38.55%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,26 +1,32 @@
 class Solution {
-    public int solve(int row, int col, int[][] grid , int[][] dp){
 
-        if(row == grid.length-1 && col == grid[0].length-1)
+    public int solve(int row, int col, int[][] grid, int[][] dp) {
+
+        if (row == grid.length - 1 && col == grid[0].length - 1)
             return grid[row][col];
 
-        if(row >= grid.length || col >= grid[0].length)
-            return Integer.MAX_VALUE;
+        if (row >= grid.length || col >= grid[0].length)
+            return (int)1e9;
 
-        if(dp[row][col] != -1) return dp[row][col];    
+        if (dp[row][col] != -1)
+            return dp[row][col];
 
-        int right = solve(row, col+1, grid,dp);
-        int down  = solve(row+1, col, grid,dp);
+        int right = solve(row, col + 1, grid, dp);
+        int down = solve(row + 1, col, grid, dp);
 
         return dp[row][col] = grid[row][col] + Math.min(right, down);
     }
 
     public int minPathSum(int[][] grid) {
+
         int n = grid.length;
         int m = grid[0].length;
-        int[][] dp = new int[n][m];
-        for(int[] row : dp) Arrays.fill(row,-1);
 
-        return solve(0,0,grid,dp);
+        int[][] dp = new int[n][m];
+
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return solve(0, 0, grid, dp);
     }
 }
