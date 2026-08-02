8f65b402560e0f470754101bb9b604463a551cb8
Harshit Pundir <harshitpundir36@gmail.com>
2026-08-02 05:37:17 +0530

    Time: 0 ms (100%), Space: 43 MB (62.03%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,18 +1,6 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        Arrays.sort(piles);
-
-        int alice =0;
-        int bob = 0;
-
-        int i = piles.length-1;
-        while(i >= 0){
-            alice += piles[i];
-            i--;
-            bob += piles[i];
-            i--;
-        }
-
-        return (alice > bob) ? true : false;
+        
+        return true ;
     }
 }
