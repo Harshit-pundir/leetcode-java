e8cb6252569a4a8dc936ebef67dba2ce43f30b4a
Harshit Pundir <harshitpundir36@gmail.com>
2026-08-05 09:24:12 +0530

    Time: 1 ms (30.86%), Space: 42.4 MB (48.15%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int setbit = 0;
+        while(n != 0){
+            if((n & 1) == 1) setbit++;
+            n >>= 1;
+        }
+        
+        return setbit;
+    }
+}
