ba9cfd180dc1279156ed9084614336f6258609af
Harshit Pundir <harshitpundir36@gmail.com>
2026-08-05 19:58:20 +0530

    Time: 59 ms (82.69%), Space: 290 MB (42.31%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for(int i =0; i<n; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int[] edge : invocations){
+            graph.get(edge[0]).add(edge[1]);
+        }
+
+        boolean[] suspicious = new boolean[n];
+
+        dfs(graph,k,suspicious);
+
+        for(int[] edge : invocations){
+            int u = edge[0];
+            int v = edge[1];
+
+            if(!suspicious[u] && suspicious[v]){
+                List<Integer> ans = new ArrayList<>();
+                for(int i =0; i<n; i++){
+                    ans.add(i);
+                }
+
+                return ans;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+    public void dfs(List<List<Integer>> graph , int node , boolean[] suspicious){
+        suspicious[node] = true;
+
+        for(int neigh : graph.get(node)){
+            if(!suspicious[neigh]){
+                dfs(graph,neigh,suspicious);
+            }
+        }
+    }
+}
