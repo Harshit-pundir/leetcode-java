bca5766b16971896f6b59f53ceb19ba98b4bd07a
Harshit Pundir <harshitpundir36@gmail.com>
2026-08-06 05:42:58 +0530

    Time: 15 ms (38.68%), Space: 57.6 MB (6.79%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,35 +1,31 @@
 class Solution {
-    public int solve(int idx, int amount, int[] coins , int[][] dp){
-        if(idx == 0){
-            return (amount % coins[idx] == 0) ? 1 : 0;
-        }
-        if(dp[idx][amount] != -1) return dp[idx][amount];
-        int notPick = solve(idx-1,amount,coins,dp);
-        int pick = 0;
-        if(coins[idx] <= amount) pick = solve(idx,amount-coins[idx],coins,dp);
+    public int solve(int idx, int amount, int[] coins, int[][] dp) {
 
-        return dp[idx][amount] = notPick + pick;
+        // Base case
+        if (idx == 0) {
+            return (amount % coins[0] == 0) ? 1 : 0;
+        }
+
+        if (dp[idx][amount] != -1)
+            return dp[idx][amount];
+
+        int notTake = solve(idx - 1, amount, coins, dp);
+
+        int take = 0;
+        if (coins[idx] <= amount) {
+            take = solve(idx, amount - coins[idx], coins, dp);
+        }
+
+        return dp[idx][amount] = take + notTake;
     }
+
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n][amount+1];
-        
-        for(int sum =0; sum <= amount ; sum++){
-            if(sum % coins[0] == 0) dp[0][sum] = 1;
-        }
-        for(int idx = 0; idx< n; idx++) dp[idx][0] = 1;
-        
-        for(int idx = 1; idx < n; idx++){
-            for(int sum =0; sum <= amount; sum++){
-                int notPick = dp[idx-1][sum];
-                int pick = 0;
-                if (coins[idx] <= sum)
-                    pick = dp[idx][sum - coins[idx]];
-                
-                dp[idx][sum] = pick + notPick;
-            }
-        }
-        
-        return dp[n-1][amount];
+
+        int[][] dp = new int[n][amount + 1];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return solve(n - 1, amount, coins, dp);
     }
 }
