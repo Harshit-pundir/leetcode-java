2c0d5776f59c899c0de9fa346ae923cc570f4a15
Harshit Pundir <harshitpundir36@gmail.com>
2026-08-06 09:53:47 +0530

    Time: 2 ms (96.31%), Space: 45.7 MB (68.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -7,15 +7,15 @@ class Solution {
 
         if(idx == candidates.length || currSum > target) return;
 
+        //not pick
+        solver(candidates, target,currSum,idx+1,list,ans);
+
         // pick
         if(currSum + candidates[idx] <= target){
             list.add(candidates[idx]);
             solver(candidates,target,currSum + candidates[idx],idx,list,ans);
             list.remove(list.size()-1);
         }
-
-        //not pick
-        solver(candidates, target,currSum,idx+1,list,ans);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
