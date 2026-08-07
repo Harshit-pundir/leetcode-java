77793dc63381db4a4b403050aeb1a9609eccd205
Harshit Pundir <harshitpundir36@gmail.com>
2026-08-07 05:40:36 +0530

    Time: 0 ms (100%), Space: 42 MB (66.67%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int paths(int m, int n, int[][] dp) {
+        if(n == 1 && m == 1) return 1;
+        if(n == 0 || m == 0) return 0;
+
+        if(dp[m][n] != -1) return dp[m][n];
+        int rightWays = paths(m,n-1,dp);
+        int downWays = paths(m-1,n,dp);
+        
+        return dp[m][n] = rightWays + downWays;
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i =0; i<=m ; i++){
+            Arrays.fill(dp[i],-1);
+        }
+
+        return paths(m,n,dp);
+    }
+}
