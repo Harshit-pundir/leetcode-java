539ceaa28472f605da86a52866d99c8670362ed7
Harshit Pundir <harshitpundir36@gmail.com>
2026-08-07 06:00:11 +0530

    Time: 2 ms (88.76%), Space: 47.6 MB (96.2%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,22 +1,16 @@
 class Solution {
-    public boolean solve(int idx , int[] nums , int[] dp){
-        if(idx >= nums.length-1) return true;
-        if(nums[idx] == 0 ) return false;
-
-        if(dp[idx] != -1) return (dp[idx] == 0) ? false : true;
-
-        for(int jump = 1; jump <= nums[idx] ; jump++){
-            if(solve(idx + jump , nums,dp)) {
-                dp[idx] = 1;
-                return true;
-            }
-        }
-        dp[idx] = 0;
-        return false;
-    }
     public boolean canJump(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-        return solve(0,nums,dp);
+
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i > maxReach)
+                return false;
+
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
     }
 }
