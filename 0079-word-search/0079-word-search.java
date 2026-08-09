13ea4db11c24a26f881ca701de162a5fe60fc979
Harshit Pundir <harshitpundir36@gmail.com>
2026-08-09 12:49:59 +0530

    Time: 134 ms (57.39%), Space: 43.2 MB (28.7%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for(int i =0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(dfs(board ,word ,  i , j , 0)) return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean dfs(char[][] board, String word , int i , int j , int idx){
+        if(idx == word.length()) return true;
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(idx)) return false;
+
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        boolean down = dfs(board, word, i + 1, j, idx + 1);
+        boolean up = dfs(board, word, i - 1, j, idx + 1);
+        boolean right = dfs(board, word, i, j + 1, idx + 1);
+        boolean left = dfs(board, word, i, j - 1, idx + 1);
+
+        boolean found = down || up || right || left;
+
+        board[i][j] = temp;
+
+        return found;
+    }
+}
