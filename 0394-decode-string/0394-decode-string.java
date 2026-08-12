fb6ed761a9893dbdbd72df65041bc6f5ddb1022d
Harshit Pundir <harshitpundir36@gmail.com>
2026-08-12 05:42:32 +0530

    Time: 3 ms (22.17%), Space: 43.2 MB (8.34%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String decodeString(String s) {
+
+        Stack<Integer> numStack = new Stack<>();
+        Stack<String> strStack = new Stack<>();
+
+        String curr = "";
+        int num = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+
+            else if (ch == '[') {
+                numStack.push(num);
+                strStack.push(curr);
+
+                num = 0;
+                curr = "";
+            }
+
+            else if (ch == ']') {
+                int repeat = numStack.pop();
+                String prev = strStack.pop();
+
+                curr = prev + curr.repeat(repeat);
+            }
+
+            else {
+                curr += ch;
+            }
+        }
+
+        return curr;
+    }
+}
