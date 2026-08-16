c0ca5f0194b5d4d9e73165bd666986858a45893f
Harshit Pundir <harshitpundir36@gmail.com>
2026-08-16 05:54:35 +0530

    Time: 2 ms (87.8%), Space: 47.4 MB (62.08%) - LeetHub

---
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int max = Integer.MIN_VALUE;
+            int size = q.size();
+
+            for(int i =0; i <size; i++){
+                TreeNode curr = q.poll();
+                max = Math.max(curr.val,max);
+
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
+            }
+
+            ans.add(max);
+        }
+        return ans;
+    }
+}
