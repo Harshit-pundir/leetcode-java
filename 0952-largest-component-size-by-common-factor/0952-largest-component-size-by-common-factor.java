dfaa17b6f8ce0fca2a2862d68a7d8448dbb4540c
Harshit Pundir <harshitpundir36@gmail.com>
2026-09-01 04:59:32 +0530

    Time: 105 ms (57.9%), Space: 51.1 MB (38.89%) - LeetHub

---
+++ 0952-largest-component-size-by-common-factor/0952-largest-component-size-by-common-factor.java
@@ -0,0 +1,63 @@
+class Solution {
+    int[] parent;
+    int[] size;
+
+    public int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    public void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if (pa == pb) return;
+
+        if (size[pa] < size[pb]) {
+            parent[pa] = pb;
+            size[pb] += size[pa];
+        } else {
+            parent[pb] = pa;
+            size[pa] += size[pb];
+        }
+    }
+
+    public int largestComponentSize(int[] nums) {
+        int max = 0;
+
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+
+        parent = new int[max + 1];
+        size = new int[max + 1];
+
+        for (int i = 0; i <= max; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+
+        // Connect every number with its factors
+        for (int num : nums) {
+            for (int factor = 2; factor * factor <= num; factor++) {
+                if (num % factor == 0) {
+                    union(num, factor);
+                    union(num, num / factor);
+                }
+            }
+        }
+
+        // Count only the numbers present in nums
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+
+        for (int num : nums) {
+            int root = find(num);
+
+            map.put(root, map.getOrDefault(root, 0) + 1);
+            ans = Math.max(ans, map.get(root));
+        }
+
+        return ans;
+    }
+}
