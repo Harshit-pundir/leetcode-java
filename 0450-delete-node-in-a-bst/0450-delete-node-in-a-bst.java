393bbc8265ded76d72e649da0720dd754af832e7
Harshit Pundir <harshitpundir36@gmail.com>
2026-09-08 06:02:10 +0530

    Time: 0 ms (100%), Space: 47.3 MB (76.29%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return null;
+
+        if(root.val < key) root.right = deleteNode(root.right, key); 
+        else if(root.val > key) root.left = deleteNode(root.left, key);
+        else{
+            if(root.left == null) return root.right;
+            else if(root.right == null) return root.left;
+            else{
+                TreeNode minVal = getMin(root.right);
+                root.val = minVal.val;
+                root.right = deleteNode(root.right,minVal.val);
+            }
+            
+        }
+
+        return root;
+    }
+
+    public TreeNode getMin(TreeNode root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+}
