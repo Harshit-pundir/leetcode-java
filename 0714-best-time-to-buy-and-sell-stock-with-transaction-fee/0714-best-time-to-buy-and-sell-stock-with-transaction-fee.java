f3aa3db2c90b64b79aceb7c0a318f042d33123f9
Harshit Pundir <harshitpundir36@gmail.com>
2026-09-09 10:31:29 +0530

    Time: 5 ms (81.89%), Space: 64.9 MB (97.86%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+
+        int buy = -prices[0];
+        int sell = 0;
+
+        for(int i = 1; i < n; i++){
+            buy = Math.max(buy, sell - prices[i]);
+            sell = Math.max(sell, buy + prices[i] - fee);
+        }
+
+        return sell;
+    }
+}
