81fc042b258ff4950a50e2174636aec7ee86aab8
Harshit Pundir <harshitpundir36@gmail.com>
2026-09-13 19:16:54 +0530

    Time: 1 ms (99.75%), Space: 45.3 MB (32.01%) - LeetHub

---
+++ 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraySum(int[] nums) {
+        int totalSum= 00;
+
+        for(int i = 0; i < nums.length; i++){
+            int start = Math.max(0,i-nums[i]);
+            int sum = 0;
+            for(int j = start; j <=i; j++){
+                sum += nums[j];
+            }
+            totalSum += sum;
+        }
+
+        return totalSum;
+    }
+}
