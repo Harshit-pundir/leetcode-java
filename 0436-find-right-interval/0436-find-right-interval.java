4654e1bfc6b789cea3273b47706c58f8b1e4e332
Harshit Pundir <harshitpundir36@gmail.com>
2026-09-15 15:19:03 +0530

    Time: 11 ms (96.62%), Space: 54.1 MB (27.25%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.java
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -1,20 +1,36 @@
+import java.util.*;
+
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
         int n = intervals.length;
+
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = intervals[i][0];
+            arr[i][1] = i;
+        }
+
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+
         int[] ans = new int[n];
 
         for (int i = 0; i < n; i++) {
             int end = intervals[i][1];
 
-            int minStart = Integer.MAX_VALUE;
+            int low = 0;
+            int high = n - 1;
             int index = -1;
 
-            for (int j = 0; j < n; j++) {
-                int start = intervals[j][0];
+            
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
 
-                if (start >= end && start < minStart) {
-                    minStart = start;
-                    index = j;
+                if (arr[mid][0] >= end) {
+                    index = arr[mid][1];
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
                 }
             }
 
