55dad9aa61375f5b0b552d0302cac53f248db53d
Harshit Pundir <harshitpundir36@gmail.com>
2026-09-15 10:42:41 +0530

    Time: 2 ms (44.48%), Space: 42.8 MB (45.74%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,14 +1,24 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int count = 0;
-        long sum = 0;
-        for(int i =1; i <= n; i++){
-            if((long)sum + i <= n){
-                sum +=i;
-                count++;
-            }else break;
+        long low = 1;
+        long high = n;
+
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+
+            long sum = mid * (mid + 1) / 2;
+
+            if (sum == n) {
+                return (int) mid;
+            } 
+            else if (sum < n) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
         }
 
-        return count;
+        return (int) high;
     }
 }
