10329abdb35d8f5e79d7c3eaf22aa52e69cef1b9
Harshit Pundir <harshitpundir36@gmail.com>
2026-09-19 06:23:42 +0530

    Time: 0 ms (100%), Space: 42 MB (74.08%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int low = 1;
+        int high = num;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if((long) mid * mid == num) return true;
+            if((long) mid * mid < num) low = mid + 1;
+            else high = mid-1;
+        }
+
+        return false;
+    }
+}
