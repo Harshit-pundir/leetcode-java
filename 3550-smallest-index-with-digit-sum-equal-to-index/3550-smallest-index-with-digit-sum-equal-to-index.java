076bcfdf1098ce5a6389b6fef4c88747c1ddc5a5
Harshit Pundir <harshitpundir36@gmail.com>
2026-09-24 05:41:35 +0530

    Time: 1 ms (99.69%), Space: 45.5 MB (59.67%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int smallestIndex(int[] nums) {
+        int i = 0;
+        for(int num : nums){
+            int sum = 0;
+            while(num != 0){
+                sum += num %10;
+                num /= 10;
+            }
+
+            if(i == sum) return i;
+            i++;
+        }
+        return -1;
+    }
+}
